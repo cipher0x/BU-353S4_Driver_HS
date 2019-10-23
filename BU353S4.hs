213e9e37315913401ad3c46e96ae0948746de987
Eric Taylor <cipher0x@gmail.com>
2019-10-22 20:57:54 -0400

    create function to find a return IO String for line

--- BU353S4.hs
+++ BU353S4.hs
@@ -52,9 +52,17 @@ get_reported_checksum_subList xs = [xs!!((indexOfAsterisk $ stringToIntList xs)+
 get_reported_checksum :: [Char]->Int
 get_reported_checksum xs = parseHex  $ get_reported_checksum_subList xs
 
+--find sub string in string
+find_string :: (Eq a) => [a] -> [a] -> Int
+find_string search str = fromMaybe (-1) $ findIndex (isPrefixOf search) (tails str)
+
+--is GPGGA line
+is_GPGGA_line :: [Char]->Bool
+is_GPGGA_line xs = if (find_string "GPGGA" xs) == (-1) then False else True 
+
 --validate GPGGA line
 validate_GPGGA_line :: [Char]->Bool
-validate_GPGGA_line xs = if contain_asterisk xs then if get_reported_checksum xs == str_GPGGA_checksum xs then True else False else False
+validate_GPGGA_line xs = if contain_asterisk xs && is_GPGGA_line xs then if get_reported_checksum xs == str_GPGGA_checksum xs then True else False else False
 --if get_reported_checksum xs == str_GPGGA_checksum xs then True else False
 
 --conatins aserisk
@@ -72,20 +80,33 @@ clean_print :: String -> IO ()
 clean_print str | str == ""  = putChar '\r'
                 | otherwise = print str
 
-main = do
-  let port = "/dev/ttyUSB0"  -- Linux
-  s <- hOpenSerial port defaultSerialSettings {commSpeed = CS4800,
-                                              timeout = 5000}
+--get a valid GPGGA line
+get_valid_line :: IO String
+get_valid_line = do --pass s into this function******************
+    let port = "/dev/ttyUSB0"  -- Linux
+    s <- hOpenSerial port defaultSerialSettings {commSpeed = CS4800,
+                                                timeout = 5000}
+    ln <- hGetLine s
+    if validate_GPGGA_line ln then return ln else get_valid_line 
+
+main :: IO ()
+main = do 
+  valid_line <- get_valid_line
+  print(valid_line)
+  --let port = "/dev/ttyUSB0"  -- Linux
+  --s <- hOpenSerial port defaultSerialSettings {commSpeed = CS4800,
+  --                                            timeout = 5000}
                                               --- The Timeout is given in tenth of seconds. 
                                               --- This means, that the program waits 100 ms for a
                                               --- character, until it throws an "End of File reached" error
   --replicateM_ 100 (hGetLine s >>= (clean_print . get_GPGGA_line))
-  ln <- hGetLine s
-  unless (validate_GPGGA_line ln) $ do
+  --ln <- hGetLine s
+  --unless (validate_GPGGA_line ln) $ do
   --when (not $ validate_GPGGA_line ln) $ do
-  print(ln)
-  main
-  print("OutSide")
-  print(ln)
+  --print(ln)
+  --main
+  --ln <- hGetLine s
+  --print("OutSide")
+  --print(ln)
   --if validate_GPGGA_line ln then print(ln) else print("Invalid Line")
-  hClose s
+  --hClose s
